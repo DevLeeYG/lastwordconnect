42aebe5f8c37f7b6efbbc6607ce05bebc9c0952f
hi33741 <hi33741@naver.com>
2021-09-30 20:27:44 +0900

    dompractice

---
+++ index.js
@@ -0,0 +1,6 @@
+const number = prompt("몇명이 참가합니까");
+const changeNumber = Number(number);
+const $input = document.querySelector("input");
+const $button = document.querySelectorAll("button");
+
+console.log($button);
